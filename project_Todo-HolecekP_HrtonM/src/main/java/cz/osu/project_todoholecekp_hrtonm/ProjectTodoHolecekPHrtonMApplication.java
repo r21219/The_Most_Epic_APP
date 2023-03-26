39944d709dc879e20488d1feb8951953f372337d
SealCandy <pavlikholecek2@email.cz>
2023-03-26 18:22:18 +0200

    Initial commit - contains the bare bones

---
+++ project_Todo-HolecekP_HrtonM/src/main/java/cz/osu/project_todoholecekp_hrtonm/ProjectTodoHolecekPHrtonMApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.project_todoholecekp_hrtonm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTodoHolecekPHrtonMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTodoHolecekPHrtonMApplication.class, args);
+	}
+
+}
---
+++ project_Todo-HolecekP_HrtonM/src/test/java/cz/osu/project_todoholecekp_hrtonm/ProjectTodoHolecekPHrtonMApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.project_todoholecekp_hrtonm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTodoHolecekPHrtonMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
